201e4b9dbda8a41a901b7e03558c075e2f3290fb
Tharindu Thennakoon <chanaka152@gmail.com>
2023-12-13 01:55:36 +0530

    Add new project

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,7 +17,6 @@ import Angular from "../assets/img/angular.png"
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
